85ed86246eeca2c2894b79826ad2295a0203da1b
Safayet <hossainsafayet187@gmail.com>
2024-12-26 01:37:50 +0600

    top selling section added

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import NewArrival from "./components/NewArrival";
+import TopSelling from "./components/TopSelling";
 
 export default function Home() {
   return (
   <main>
    <NewArrival />
+   <TopSelling/>
   </main>
   );
 }
