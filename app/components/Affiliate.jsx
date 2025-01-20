19ead7177612ca88512cb55799c432935639db65
safayet404 <hossainsafayet187@gmail.com>
2025-01-20 20:12:20 +0600

    affiliate text make litte smaller

--- app/components/Affiliate.jsx
+++ app/components/Affiliate.jsx
@@ -6,7 +6,7 @@ const Affiliate = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 p-10 gap-10">
             <div>
                 <h1 className="mt-10 font-bold  md:text-xl lg:text-4xl uppercase  text-black"> Join our affiliate team </h1>
-                <p className="text-[#5C5C5C] mt-5 lg:text-base md:text-sm "> Success in affiliate marketing is not just about selling products, 
+                <p className="text-[#5C5C5C] mt-5 text-xs lg:text-base md:text-sm "> Success in affiliate marketing is not just about selling products, 
                     it’s about building trust and providing value. 
                     When you help others find what they need, success will follow.</p>
                 <button className="bg-[#D9D9D9] px-4 py-2 mt-5 font-semibold rounded-md  text-black">Get Started Today</button>
