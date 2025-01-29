d2821e9d8fc7b4c90dbfb0148c1ba8d167273e21
safayet404 <hossainsafayet187@gmail.com>
2025-01-29 21:37:53 +0600

    revalidate in 10 second

--- app/page.js
+++ app/page.js
@@ -12,17 +12,15 @@ export default async function Home() {
   const products = productsData.map((edge) => edge.node);
   const topSellingProducts = productsData1.map((edge) => edge.node);
 
-  
-
   return (
     <main>
       <ImageSlider />
-      <CommonComponentWrapper title="New Arrival" products={products}  />
-      <CommonComponentWrapper title="Top Selling" products={topSellingProducts}/>
-      
-     
+      <CommonComponentWrapper title="New Arrival" products={products} />
+      <CommonComponentWrapper title="Top Selling" products={topSellingProducts} />
       <DressStyle />
       <Affiliate />
     </main>
   );
 }
+
+export const revalidate = 10; // Revalidate every 10 seconds
